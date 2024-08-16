bc479baf2339422f520158645397357d0738947c
xiezipei <zipeixie@gmail.com>
2024-08-16 16:00:34 +0800

    feat: 封装成可执行命令行

---
+++ index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+
+function deleteNodeModules() {
+  const nodeModulesPath = path.join(process.cwd(), 'node_modules');
+
+  fs.rm(nodeModulesPath, { recursive: true, force: true }, (err) => {
+    if (err) {
+      console.error(`Failed to delete node_modules: ${err.message}`);
+    } else {
+      console.log('node_modules directory has been deleted successfully.');
+    }
+  });
+}
+
+module.exports = deleteNodeModules;
